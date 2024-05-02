6f7143e0bbf7bfd8f9b4820aec579e3f0c059dac
Lizunchik777 <liza.balaban2009@gmail.com>
2024-05-02 20:04:39 +0300

    dz-ex1

--- main.cpp
+++ main.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int main()
 {
-
+	cout << "enter num";
+	int num1;
+	cin >> num1;
+	int num2 = 500;
+	int i = num1;
+	int sum = 0;
+	while (i <= num2)
+	{
+		sum += i;
+		i++;
+	}
+	cout << sum << endl;
 	system("pause");
 	return 0;
 }
