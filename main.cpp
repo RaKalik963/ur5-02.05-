23707aa1a6842f2d723d6b69a4a168c4c47aeea0
Lizunchik777 <liza.balaban2009@gmail.com>
2024-05-02 19:46:53 +0300

    lab-ex3

--- main.cpp
+++ main.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main()
 {
-
+	cout << "enter first num";
+	int num1;
+	cin >> num1;
+	cout << "enter last num";
+	int num2;
+	cin >> num2;
+	int i = num1;
+	int sum = 0;
+	while (i <= num2)
+	{
+		sum += i;
+		i++;
+	}
+	cout << sum << endl;
 	system("pause");
 	return 0;
 }
