c4339831285f9ec83ee6317ffc0b2c99b7a1b208
Lizunchik777 <liza.balaban2009@gmail.com>
2024-05-02 20:16:33 +0300

    dz-ex2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main()
 {
-
+	cout << "enter x" << endl;
+	int x;
+	cin >> x;
+	cout << "enter y" << endl;
+	int y;
+	cin >> y;
+	int i = 1;
+	int ste = 0;
+	while (ste < y)
+	{
+		i *= x;
+		ste++;
+	}
+	cout << i << endl;
 	system("pause");
 	return 0;
 }
