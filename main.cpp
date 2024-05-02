2375b1bdcb00dbddfeff0cd2ffe8e598147ec687
Lizunchik777 <liza.balaban2009@gmail.com>
2024-05-02 19:59:09 +0300

    dorobutuuuuuuuuuu

--- main.cpp
+++ main.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 int main()
 {
+	cout << "enter first num";
+	int num1;
+	cin >> num1;
+	cout << "enter last num";
+	int num2;
+	cin >> num2;
+	int zero;
+	do
+	{
+		int sum = num1 + num2;
+		cout << "enter num(0) to see answer";
+		cin >> zero;
+		if (zero == 0)
+		{
+			cout << sum << endl;
+		}
 
+	} while (zero == 0);
 	system("pause");
 	return 0;
 }
