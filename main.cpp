689c5822737ff869ee38c3d245d39ce5f0d1b416
RaKalik963 <andrejmahot646@gmail.com>
2024-05-07 16:57:24 +0300

    dz-ex3

--- main.cpp
+++ main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main()
 {
+    int sum = 0;
+    int num = 1;
 
+    while (num <= 1000) {
+        sum += num;
+        num++;
+    }
+    float average = static_cast<float>(sum) / 1000;
+
+    cout << average << endl;
 	system("pause");
 	return 0;
 }
