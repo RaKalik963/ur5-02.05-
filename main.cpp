cd129b6aa1575612c1cbffe60430bf3d09f29338
RaKalik963 <andrejmahot646@gmail.com>
2024-05-07 17:09:23 +0300

    dz-ex5

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
-
+    cout << "enter num ";
+    int k;
+    cin >> k;
+    int i = 1;
+    while (i <= 100) {
+        cout << k << " x " << i << " = " << (k * i) << endl;
+        i++;
+    }
 	system("pause");
 	return 0;
 }
