1bb14d71e5c2f3ca1480755f9581deed79e10457
RaKalik963 <andrejmahot646@gmail.com>
2024-05-07 17:05:06 +0300

    dz-ex4

--- main.cpp
+++ main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main()
 {
-
+    cout << "enter num vid (1 <= a <= 20): ";
+    int a;
+    cin >> a;
+    int dob = 1;
+    int i = a;
+    while (i <= 20) {
+        dob *= i;
+        i++;
+    }
+    cout << dob << endl;
 	system("pause");
 	return 0;
 }
